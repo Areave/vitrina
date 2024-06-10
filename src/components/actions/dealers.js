40f450cc667f43e05cb4fd1d8061b5bb7fa58871
Хохлушин Игорь <just4-spam@mail.ru>
2024-06-10 23:53:37 +0500

    Update dealers.js

--- src/components/actions/dealers.js
+++ src/components/actions/dealers.js
@@ -1,37 +1,49 @@
 import axios from "axios";
 import {
-  setDealers,
-  setDealersLoadingError,
+    setDealers,
+    setDealersLoadingError
 } from "../../reducers/dealersRedusers";
 
 export const getDealers = () => {
 
-  // const url = `http://localhost:4200/get_kiosk_collaborators`
-  let url = `${global.config.protocol}://${global.config.apiHost}${global.config.apiPrefix ? '/' + global.config.apiPrefix : ''}/get_kiosk_additional_dealers${global.config.sid ? '?sid=' + global.config.sid : ''}`;
+    // const url = `http://localhost:4200/get_kiosk_collaborators`
+    let url = `${global.config.protocol}://${global.config.apiHost}${global.config.apiPrefix ? "/" + global.config.apiPrefix : ""}/get_kiosk_additional_dealers${global.config.sid ? "?sid=" + global.config.sid : ""}`;
 
-  return async (dispatch) => {
-    
-    axios({
-      method: "GET",
-      url: url,
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${global.config.sid}`
-      },
-    }).then((data) => {
-      console.log("цукцукцукц");
-      dispatch(setDealers(data.data.data));
-    }).catch((error) => {
-      console.log("о ш и б к а");
-      dispatch(setDealersLoadingError());
-    });
+    return async (dispatch) => {
 
-    // console.log('dealers:', response)
-    
-    // if (Array.isArray(response?.data?.data)) {
-    //     dispatch(setDealers(response.data.data));
-    // } else {
-    //   dispatch(setDealersLoadingError());
-    // }
-  };
+        try {
+            const data = await axios({
+                method: "GET",
+                url: url,
+                headers: {
+                    "Content-Type": "application/json"
+                    // "Authorization": `Bearer ${global.config.sid}`
+                }
+            });
+            if (data?.data?.data) {
+                dispatch(setDealers(data.data.data));
+            }
+        } catch (e) {
+            console.log("о ш и б к а");
+            console.log(e);
+            dispatch(setDealersLoadingError());
+        }
+
+
+        //     .then((data) => {
+        //   console.log("цукцукцукц");
+        //   dispatch(setDealers(data.data.data));
+        // }).catch((error) => {
+        //   console.log("о ш и б к а");
+        //   dispatch(setDealersLoadingError());
+        // });
+
+        // console.log('dealers:', response)
+
+        // if (Array.isArray(response?.data?.data)) {
+        //     dispatch(setDealers(response.data.data));
+        // } else {
+        //   dispatch(setDealersLoadingError());
+        // }
+    };
 };
