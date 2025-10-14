04cf35a5d5fa41ed8f35010acced10e6b9aa5341
Хохлушин Игорь <just4-spam@mail.ru>
2025-10-14 19:38:57 +0500

    new dealer

--- src/components/SalonButton.jsx
+++ src/components/SalonButton.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import logo_100czk from "../../public/img/logo_100czk.svg";
 import bc_logo from "../../public/img/bc-logo.png";
+import bca_logo from "../../public/img/bca-logo.svg";
 import { resetDealer, setDealer } from "../reducers/dealersRedusers";
 import '../../public/css/salonButton.css'
 
@@ -10,22 +11,19 @@ const SalonButton = ({dealer}) => {
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const settings = useSelector((state) => state.parameters);
 
     let {id, name, label} = dealer;
 
-    if (id === 0) {
-        name = 'main';
-    } else {
-        name = 'co-working';
+    let isBCA = settings.kiosk_name === '267 BCT Akademie - Praha - Karlovo namesti - Atrium';
+
+    if (isBCA && id === 0) {
+        label = 'BCT ACADEMIE'
     }
 
-    const labels = {
-        'co-working': 'Beauty Coworking Time',
-        'main': 'Terminál'
-    };
     const logos = {
         "co-working": bc_logo,
-        'main': logo_100czk
+        'main': isBCA ? bca_logo : logo_100czk
     };
 
     const onButtonClick = () => {
@@ -36,8 +34,8 @@ const SalonButton = ({dealer}) => {
 
 
     return <div className={'salon_button ' + name} onClick={onButtonClick}>
-        <div className={"img_container " + name}>
-            <img src={logos[name] || logo_100czk} alt="terminals"/>
+        <div className={"img_container " + name + `${isBCA ? ' square' : ''}`}>
+            <img src={logos[name]} alt="terminals"/>
         </div>
         <div className={"label " + name}>
             {label || 'Terminal'}
