8adcca758d73bfd5f45fbcb8e66ae775df20ce85
Хохлушин Игорь <just4-spam@mail.ru>
2025-12-04 18:34:27 +0700

    language support

--- src/components/actions/dealers.js
+++ src/components/actions/dealers.js
@@ -29,7 +29,7 @@ export const getKioskSettings = (setIsLoading) => {
                 dispatch(setKioskLoadingError(data?.data?.data.kiosk_error));
             }
             else if (data?.data?.data) {
-                dispatch(setKioskSettings({...data?.data.data, language_memo: data?.data.data.language_memo.toLowerCase()}));
+                dispatch(setKioskSettings({...data?.data.data, language_memo: data?.data.data.language_memo?.toLowerCase() || 'cz'}));
                 setIsLoading && setIsLoading(false)
             }
         } catch (e) {
